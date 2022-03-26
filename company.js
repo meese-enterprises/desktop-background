4ad16d343aec38472b9f3013b62c6b683ecf0ecb
Aaron Meese <ajmeese7@gmail.com>
2022-03-25 23:17:05 -0400

    Proof of Concept
    When I implement the Wallpaper Engine settings file, something goes wrong and the animation refuses to initialize unless I manually instantiate the class again from the console. So once that is resolved this will be nearly completely finished.

---
+++ company.js
@@ -0,0 +1,145 @@
+/**
+ * Rearranges the indices of an array and returns the new array.
+ * @returns {Array}
+ */
+Array.prototype.shuffle = function () {
+	const input = this;
+
+	for (let i = input.length - 1; i >= 0; i--) {
+		const randomIndex = Math.floor(Math.random() * (i + 1));
+		const itemAtIndex = input[randomIndex];
+
+		input[randomIndex] = input[i];
+		input[i] = itemAtIndex;
+	}
+
+	return input;
+};
+
+/** Gets a random integer between the two specified values. */
+const getRandomInt = (min = 0, max = 1) =>
+	Math.floor(Math.random() * (max - min)) + min;
+
+/**
+ * Modified from https://codepen.io/blazicke/pen/dQjxMr
+ */
+class ScrambleLetters {
+	constructor(selector = ".scramble") {
+		console.log("ScrambleLetters STARTED...");
+		this.selector = selector;
+		this.isScrambling = false;
+		this.repeat = 0;
+		this.target = [];
+		this.letters = "*+-/@_$[%£!XO1&>";
+		this.originalStrings = [];
+		this.singleLetters = [];
+		this.initialDelay = getRandomInt(400, 750);
+		this.durationInterval = () => getRandomInt(250, 450);
+		this.animationInterval = () => getRandomInt(8500, 14500);
+
+		this.init();
+	};
+
+	init() {
+		if (!this.checkLength(this.selector)) {
+			setTimeout(this.init.bind(this), 500);
+		}
+
+		this.targets = Array.from(document.querySelectorAll(this.selector));
+		this.divideLetters();
+
+		// Run the scramble animation after the initial delay
+		setTimeout(() => {
+			this.startScramble();
+		}, this.initialDelay);
+	};
+
+	startScramble() {
+		this.changeLetters();
+		this.isScrambling = true;
+
+		// Stops the scramble animation after the duration interval
+		setTimeout(() => {
+			this.isScrambling = false;
+			this.resetLetters();
+			clearTimeout(this);
+
+			// Repeat the scramble animation after the animation interval
+			setTimeout(() => {
+				this.startScramble();
+			}, this.animationInterval());
+		}, this.durationInterval());
+	}
+
+	/** Returns how many elements on the page match the specified selector. */
+	checkLength(selector) {
+		return Array.from(document.querySelectorAll(selector)).length > 0;
+	};
+
+	changeLetter(letter) {
+		if (letter.textContent != " ") {
+			letter.classList.add("is-changing");
+			letter.style.animationDuration = Math.random().toFixed(2) + "s";
+
+			const newChar = this.letters.substr(
+				Math.random() * this.letters.length,
+				1
+			);
+			letter.textContent = newChar;
+			letter.setAttribute("data-txt", newChar);
+		}
+	};
+
+	resetLetter(letter) {
+		const originalValue = letter.getAttribute("data-original");
+		letter.classList.remove("is-changing");
+		letter.textContent = originalValue;
+		letter.setAttribute("data-txt", originalValue);
+	};
+
+	divideLetters() {
+		this.targets.forEach((element, index) => {
+			const text = element.textContent;
+			let textDivided = "";
+
+			this.originalStrings[index] = text;
+
+			for (let i = 0; i < text.length; i++) {
+				textDivided += `<span class="scrambling scramble-${index}-span-${i}" data-txt="${text.charAt(
+					i
+				)}" data-original="${text.charAt(i)}">${text.charAt(i)}</span>`;
+			}
+
+			element.innerHTML = textDivided;
+		});
+
+		this.singleLetters = document.querySelectorAll(".scrambling");
+	};
+
+	changeLetters() {
+		if (this.isScrambling) {
+			this.singleLetters.forEach((el, index) => {
+				this.changeLetter(el);
+			});
+		}
+
+		setTimeout(this.changeLetters.bind(this), 10);
+	};
+
+	/** Reset to initial letters */
+	resetLetters() {
+		const randomArray = [];
+		for (let i = 0; i < this.singleLetters.length; i++) {
+			randomArray.push(i);
+		}
+
+		randomArray.shuffle();
+		randomArray.forEach((el, index) => {
+			setTimeout(() => {
+				this.resetLetter(this.singleLetters[el]);
+			}, index * 20 * (Math.random() * 5)).toFixed(2);
+		});
+	};
+};
+
+window.onload = () => new ScrambleLetters();
---
+++ wallpaper-engine.js
@@ -0,0 +1,59 @@
+/** Converts the WP Engine decimal format to standard RGB values. */
+const convertToRGB = (value) =>
+	value.split(' ').map(color => Math.ceil(color * 255));
+
+/* ################################################
+			Wallpaper Engine Interaction
+	 ################################################ */
+window.wallpaperPropertyListener = {
+	applyUserProperties: function(properties) {
+		/** Used to update CSS variables. */
+		const root = document.querySelector(":root");
+
+		if (properties.companyName) {
+			let text = properties.companyName.value;
+			document.getElementById("companyName").innerText = text;
+		}
+		if (properties.companyType) {
+			let text = properties.companyType.value;
+			document.getElementById("companyType").innerText = text;
+		}
+
+		if (properties.companyNameSize) {
+			let textSize = properties.companyNameSize.value + "px";
+			document.getElementById("companyName").style.fontSize = textSize;
+		}
+		if (properties.companyTypeSize) {
+			let textSize = properties.companyTypeSize.value + "px";
+			document.getElementById("companyType").style.fontSize = textSize;
+		}
+
+		if (properties.primaryColor) {
+			const primaryColor = convertToRGB(properties.primaryColor.value);
+
+			root.style.setProperty(
+				"--primary-color",
+				`rgba(${primaryColor}, ${properties.colorOpacity.value})`
+			);
+		}
+		if (properties.secondaryColor) {
+			const secondaryColor = convertToRGB(properties.secondaryColor);
+
+			root.style.setProperty(
+				"--secondary-color",
+				`rgba(${secondaryColor}, ${properties.colorOpacity.value})`
+			);
+		}
+		if (properties.backgroundColor) {
+			const backgroundColor = convertToRGB(properties.backgroundColor.value);
+			document.body.style.backgroundColor = `rgb(${backgroundColor})`;
+		}
+
+		if (properties.schemecolor) {
+			// Convert the scheme color to be applied as a CSS style
+			let schemeColor = properties.schemecolor.value.split(' ')
+				.map(c => Math.ceil(c * 255));
+			var schemeColorAsCSS = `rgb(${schemeColor})`;
+		}
+	}
+};
